042ae7a83ba218fd08c077b62f168251c33cc55c
Mason L <mason.lawlor@me.com>
2015-05-26 17:47:17 -0600

    trying to add carousel.. see jackalope.io post on carousels

---
+++ template-clients-salient.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template Name: Clients/Testimonials
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+
+<div id="fws_5564db431e321" class="wpb_row vc_row-fluid full-width-content parallax_section vertically-align-columns"  style="padding-top: 0px; padding-bottom: 0px;">
+  <div class="col span_12 light left">
+  	<div style=" background-color: rgba(28,32,43,0.5);  color: #ffffff;" class="vc_span4 wpb_column column_container col centered-text padding-5-percent" data-using-bg="true" data-padding-pos="all" data-hover-bg="" data-animation="" data-delay="0">
+  		<div class="wpb_wrapper">
+  			<div class="col span_12 testimonial_slider " data-autorotate="7000">
+          <div class="slides">
+            <blockquote>
+              <p>"It's easy to see why this is one of the most downloaded themes on themeforest. It's a real game-changer, and a breathe of fresh air if you work with WordPress a lot. "</p>
+              <span>&minus;  apawlik, Theme User</span>
+            </blockquote>
+            <blockquote>
+              <p>"I literally could not be happier that I chose to buy your theme! Your regular updates and superb attention to detail blows me away every time I visit my new site!"</p>
+              <span>&minus; Editor02, Theme User</span>
+            </blockquote>
+            <blockquote>
+              <p>"Amazing work! You didn’t just make a great looking theme, but you made one that is a pleasure to customize and not the least bit difficult to keep looking classy."</p>
+              <span>&minus; mdriess, Theme User</span>
+            </blockquote>
+          </div>
+        </div>
+  		</div>
+  	</div>
+
+  	<div style="background-color: rgba(28,32,43,0.7);" class="vc_span8 wpb_column column_container col padding-4-percent" data-using-bg="true" data-padding-pos="all" data-hover-bg="" data-animation="" data-delay="0">
+  		<div class="wpb_wrapper">
+  			<div class="carousel-wrap">
+          <div class="row carousel clients five-cols  disable-autorotate" data-max="5">
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/21.png" width="294" height="253" alt="client" />
+            </div>
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/6.png" width="294" height="253" alt="client" />
+            </div>
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/11.png" width="294" height="253" alt="client" />
+            </div>
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/43.png" width="294" height="253" alt="client" />
+            </div>
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/33.png" width="294" height="253" alt="client" />
+            </div>
+            <div>
+              <img src="http://themenectar.com/demo/salient-frostwave/wp-content/uploads/2013/06/51.png" width="294" height="253" alt="client" />
+            </div>
+          </div>
+        </div>
+  		</div>
+  	</div>
