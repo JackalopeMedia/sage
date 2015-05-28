71c746e5bd47807789d9156edb25991ee3a68cfc
Mason L <mason.lawlor@me.com>
2015-05-27 19:21:55 -0600

    added jackalope.less and formatted carousel

--- template-clients.php
+++ template-clients.php
@@ -9,60 +9,51 @@
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
+</div></div>
 
-<div class="bs-example" data-example-id="carousel-with-captions">
-    <div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
+<div class="cred-carousel clearfix" data-example-id="carousel-with-captions">
+  <div class="row">
+    <div class="carousel slide col-sm-6 col-md-4" data-ride="carousel">
       <ol class="carousel-indicators">
         <li data-target="#carousel-example-captions" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-captions" data-slide-to="1"></li>
         <li data-target="#carousel-example-captions" data-slide-to="2"></li>
       </ol>
       <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img data-src="holder.js/900x500/auto/#777:#777" alt="First slide image">
-          <div class="carousel-caption">
+        <div class="item active va-table">
+          <div class="carousel-caption va-table-cell">
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </div>
         </div>
-        <div class="item">
-          <img data-src="holder.js/900x500/auto/#666:#666" alt="Second slide image">
-          <div class="carousel-caption">
+        <div class="item va-table">
+          <div class="carousel-caption va-table-cell">
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
         </div>
-        <div class="item">
-          <img data-src="holder.js/900x500/auto/#555:#5555" alt="Third slide image">
-          <div class="carousel-caption">
+        <div class="item va-table">
+          <div class="carousel-caption va-table-cell">
             <h3>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </div>
         </div>
       </div>
-      <a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-captions" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
-  </div><!-- /example -->
-
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+    <div class="col-sm-6 col-md-8">
+      <div class="va-table">
+        <div class="va-table-cell">
+          <img class="" src="http://www.golfbidder.co.uk/images/stock/ogio_logo.gif" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- /example -->
 
 <script type="text/javascript">
-  $('.carousel').carousel()
+  $('.carousel').carousel({
+    pause: "hover"
+  })
 </script>
+
+<div class="container">
